594accf4f79085e8fa22106d4622049022fc954f
Oriol Alàs Cercós <oriolalascercos@gmail.com>
2022-12-17 17:46:15 +0100

    store

---
+++ lib/store/actions.dart
@@ -0,0 +1,27 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+class Routes {
+  static const String home = "HOME";
+}
+
+@immutable
+abstract class AppAction {
+  const AppAction();
+}
+
+class NavigateToNext extends AppAction {
+  final String destinationRoute;
+
+  const NavigateToNext(this.destinationRoute);
+}
+
+class NavigateToNextAndReplace extends AppAction {
+  final String destinationRoute;
+
+  const NavigateToNextAndReplace(this.destinationRoute);
+}
+
+class NavigateBack extends AppAction {}
+
--- lib/store/store.dart
+++ lib/store/store.dart
@@ -1,21 +1,54 @@
 import 'package:flutter/cupertino.dart';
 
 import '../connection/store.dart';
+import '../home/store.dart';
+import 'actions.dart';
 
 @immutable
 class AppState {
   final WebServiceState webserviceState;
+  final GlobalKey<NavigatorState> navigatorKey;
+  final HomeState homeState;
 
-  const AppState.init() : webserviceState = const WebServiceState.init();
+  AppState.init()
+      : webserviceState = const WebServiceState.init(),
+        navigatorKey = GlobalKey<NavigatorState>(),
+        homeState = HomeState.init();
+
+  AppState(this.webserviceState, this.navigatorKey, this.homeState);
+
+  AppState copy(
+          {WebServiceState? webServiceState,
+          GlobalKey<NavigatorState>? navigatorKey,
+          HomeState? homeState}) =>
+      AppState(webServiceState ?? webserviceState,
+          navigatorKey ?? this.navigatorKey, homeState ?? this.homeState);
 }
 
-class AppAction {
-
-}
+class AppAction {}
 
 AppState reducer(AppState state, AppAction action) {
   switch (action.runtimeType) {
     default:
       return state;
   }
+}
+
+AppState navigationReducer(AppState prev, AppAction action) {
+  switch (action.runtimeType) {
+    case NavigateToNext:
+      var newAction = action as NavigateToNext;
+      GlobalKey<NavigatorState> navKey = prev.navigatorKey
+        ..currentState!.pushNamed(newAction.destinationRoute);
+      return prev.copy(navigatorKey: navKey);
+    case NavigateToNextAndReplace:
+      var newAction = action as NavigateToNextAndReplace;
+      GlobalKey<NavigatorState> navKey = prev.navigatorKey
+        ..currentState!.pushReplacementNamed(newAction.destinationRoute);
+      return prev.copy(navigatorKey: navKey);
+    case NavigateBack:
+      GlobalKey<NavigatorState> navKey = prev.navigatorKey..currentState!.pop();
+      return prev.copy(navigatorKey: navKey);
+  }
+  return prev;
 }
