7048fbfab49af7e737a85ea829b2035ea49bf888
lauraharoescoi <lauraha.escoi@gmail.com>
2022-12-17 18:14:21 +0100

    throw

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class HomeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    throw Scaffold(
+    return Scaffold(
       body: Container(),
     );
   }
