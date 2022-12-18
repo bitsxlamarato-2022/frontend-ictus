c67e5d50882aa15970a731d0e72d6b3ef06a4d4e
MarcGSalo <marcgsmarcgs@gmail.com>
2022-12-18 01:36:15 +0100

    home

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -10,24 +10,45 @@ class HomeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: StoreConnector<AppState, VoidCallback>(
+      body:StoreConnector<AppState, VoidCallback>(
         converter: (sto) => () => sto.dispatch(NavigateToNextAndReplace(Routes.profile)),
         builder:(cto, goProfile) => Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
 
           children: <Widget>[
           const SizedBox(
-            height: 0,
+            height: 50,
           ),
-          TextButton(
+          ElevatedButton(
             style: TextButton.styleFrom(
-              textStyle: const TextStyle(fontSize: 50),
+
+              textStyle: const TextStyle(fontSize: 30),
             ),
             onPressed: goProfile,
             child: const Padding(
-              padding: EdgeInsets.fromLTRB(150, 250, 0, 0),
-              child: Text('Perfil'),//aqui se li te que passar el nom del usuari per parametre
+              padding: EdgeInsets.fromLTRB(180, 150, 0, 0),
+              child: Center(
+                child: Text('Perfil'),
+              ),//aqui se li te que passar el nom del usuari per parametre
             ),
           ),
+            const SizedBox(
+              height: 50,
+            ),
+          ElevatedButton(
+            style: TextButton.styleFrom(
+              textStyle: const TextStyle(fontSize: 50),
+              primary: Colors.grey[300],
+              ),
+            onPressed: goProfile,//TODO
+            child: const Padding(
+              padding: EdgeInsets.fromLTRB(0,0, 0, 0),
+              child: Center(
+                child: Text('Electrocardiograma'),
+              ),
+            ),
+          ),//aqui se li te que passar el nom del usuari per parametre
         ],
       ),
     ));
